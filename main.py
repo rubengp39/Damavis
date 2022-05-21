7bd9f75ff0b4be70e7d7e21d1dfc219e05bb4595
Rubén García Pedrejón <62309228+rubengp39@users.noreply.github.com>
2022-05-21 12:54:37 +0200

    Snake's next state function.

---
+++ main.py
@@ -0,0 +1,54 @@
+from asyncio.windows_events import NULL
+
+
+board = [4,3] # X, Y
+snake = [[2,2],[3,2],[3,1],[3,0],[2,0],[1,0],[0,0]] # snake[0] = head --- Pop last and add new position to head.
+depth = 3
+
+# Assuming that movement will only be one in [L,R,U,D].
+def checkCollision(board, snake, movement):
+    if(movement == "L"):
+        if(snake[0][0] > 0):
+            snakeNextX = snake[0][0] + (-1)
+            snakeNext = [snakeNextX, snake[0][1]]
+            snake.pop()
+            if((snakeNext not in snake)):
+                snake.insert(0, snakeNext)
+                return snake
+            
+    elif(movement == "R"):
+        if(snake[0][0] < board[0]-1):
+            snakeNextX = snake[0][0] + 1
+            snakeNext = [snakeNextX, snake[0][1]]
+            snake.pop()
+            if((snakeNext not in snake)):
+                snake.insert(0, snakeNext)
+                return snake
+
+    elif(movement == "U"):
+        if(snake[0][1] > 0):
+            snakeNextY = snake[0][1] + (-1)
+            snakeNext = [snake[0][0], snakeNextY]
+            snake.pop()
+            if((snakeNext not in snake)):
+                snake.insert(0, snakeNext)
+                return snake
+
+    elif(movement == "D"):
+        if(snake[0][1] < board[1]-1):
+            snakeNextY = snake[0][1] + 1
+            snakeNext = [snake[0][0], snakeNextY]
+            snake.pop()
+            if((snakeNext not in snake)):
+                snake.insert(0, snakeNext)
+                return snake
+    return NULL
+
+
+def numberOfAvailableDifferentPaths(board, snake, depth):
+    solution = 0
+    snakeHead = snake[0]
+
+    return solution
+
+print(checkCollision(board, snake, "U"))
